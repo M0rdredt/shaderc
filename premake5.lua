534fb1f0f915709c7a1071727cbe35a7a68a9d6d
Willi <Willi.Hertel@web.de>
2022-06-18 15:38:02 +0200

    Added premake build

---
+++ premake5.lua
@@ -0,0 +1,49 @@
+project "shaderc"
+	kind "StaticLib"
+	language "C"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"libshaderc/src/shaderc_private.h",
+		"libshaderc/src/shaderc.cc",
+		"libshaderc_util/src/args.cc",
+		"libshaderc_util/src/compiler.cc",
+		"libshaderc_util/src/file_finder.cc",
+		"libshaderc_util/src/message.cc",
+		"libshaderc_util/src/resources.cc",
+		"libshaderc_util/src/shader_stage.cc",
+		"libshaderc_util/src/spirv_tools_wrapper.cc",
+		"libshaderc_util/src/version_profile.cc",
+
+		"libshaderc_util/include/libshaderc_util/**.h",
+	}
+
+	includedirs
+    {
+		"libshaderc_util/include",
+		"libshaderc/include",
+		"third_party/glslang",
+		"third_party/spirv-tools/include",
+		"third_party/spirv-headers/include"
+	}
+
+	defines{"ENABLE_HLSL"}
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
